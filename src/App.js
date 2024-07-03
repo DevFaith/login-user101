e3fdcdf7e4db3e155c81aa571894e1cfd7581d49
DevFaith <waitherafaith@outlook.com>
2024-07-04 00:26:12 +0300

    * chore(App.js): refactor App component to use react-router-dom for routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import Logout from './pages/Logout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/logout" element={<Logout />} />
+          {/* Add a default route or redirect as needed */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
